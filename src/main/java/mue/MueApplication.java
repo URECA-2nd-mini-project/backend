70ad2813630d2aaff2e09b4c5d85c3150cfbe22b
shinhm1 <shinhm1@naver.com>
2024-09-25 02:07:49 +0900

    feat: 개발 환경 세팅 및 application.properties DB 연결 확인 완료

---
+++ src/main/java/mue/MueApplication.java
@@ -0,0 +1,12 @@
+package mue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MueApplication.class, args);
+	}
+}
