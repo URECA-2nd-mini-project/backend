5726b5962efea91b32a10b59f8ae3c225c4a666d
ummfieg <ummfieg@naver.com>
2024-09-25 17:43:22 +0900

    feat: Emotion, PlaylistRepository 추가 완료

--- src/main/java/mue/repository/EmotionLogRepository.java
+++ src/main/java/mue/repository/EmotionLogRepository.java
@@ -1,8 +1,15 @@
 package mue.repository;
 
 import mue.entity.*;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // EmotionLog 엔티티에 대한 CRUD 작업을 처리
 public interface EmotionLogRepository extends JpaRepository<EmotionLog, String> {
+
+    // 사용자에 대한 감정 로그 조회
+    List<EmotionLog> findByUser(User user);
+
 }
