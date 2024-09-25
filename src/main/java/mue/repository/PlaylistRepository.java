b560502c5758390c9dbda796b2e79433b6b4ec84
shinhm1 <shinhm1@naver.com>
2024-09-25 18:13:55 +0900

    feat: main 브랜치 병합

--- src/main/java/mue/repository/PlaylistRepository.java
+++ src/main/java/mue/repository/PlaylistRepository.java
@@ -1,8 +1,24 @@
 package mue.repository;
 
 import mue.entity.*;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // Playlist 엔티티에 대한 CRUD 작업을 처리
+@Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, String> {
+
+    // 사용자 ID로 모든 플레이리스트 찾기
+    List<Playlist> findByUserId(Long userId); 
+
+    // 플레이리스트 ID로 플레이리스트 세부정보 찾기
+    Optional<Playlist> findByPlaylistId(String playlistId);
+
+    // 특정 감정 태그를 가진 플레이리스트를 찾는 메소드
+    List<Playlist> findByUserIdAndEmotionTag(Long userId, String emotionTag); 
 }
+
