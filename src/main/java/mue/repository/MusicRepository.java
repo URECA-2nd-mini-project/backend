2f638068f6a2750e38ea10cd47b237362a81ed87
shinhm1 <shinhm1@naver.com>
2024-09-25 18:31:08 +0900

    feat: Music, EmotionTag Repository 작성

--- src/main/java/mue/repository/MusicRepository.java
+++ src/main/java/mue/repository/MusicRepository.java
@@ -1,8 +1,24 @@
 package mue.repository;
 
 import mue.entity.*;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 // Music 엔티티에 대한 CRUD 작업을 처리
 public interface MusicRepository extends JpaRepository<Music, String> {
+
+  // 특정 유저의 모든 플레이리스트에 속한 음악을 중복을 제거하고 조회
+  @Query("SELECT DISTINCT m FROM Music m " +
+      "WHERE m.playlist.playlistId IN " +
+      "(SELECT p.playlistId FROM Playlist p WHERE p.user.userId = :userId)")
+  List<Music> findDistinctByUserPlaylists(@Param("userId") String userId);
+
+  // 특정 플레이리스트에서 최근 재생된 음악 5개 조회
+  @Query("SELECT m FROM Music m WHERE m IN :musics ORDER BY m.playedAt DESC")
+  List<Music> findTop5ByMusicListOrderByPlayedAtDesc(@Param("musics") List<Music> musics, Pageable pageable);
 }
