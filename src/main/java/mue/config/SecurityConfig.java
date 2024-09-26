8ec5d47f43fef82e15639b0b9db4e054c2798ddc
CHOI MYEONGJI <y0uaremysumm2r@gmail.com>
2024-09-26 17:14:05 +0900

    feat: 구글 OAuth2 로그인 및 사용자 세션 처리 추가

---
+++ src/main/java/mue/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package mue.config;
+
+// 전체적인 보안 설정을 관리
+public class SecurityConfig {
+    
+}
---
+++ src/main/java/mue/service/CustomOAuth2UserService.java
@@ -0,0 +1,7 @@
+// OAuth 2.0 로그인 과정에서 사용자 정보를 가져오는 역할
+package mue.service;
+
+// 전체적인 보안 설정을 관리
+public class CustomOAuth2UserService {
+    
+}
